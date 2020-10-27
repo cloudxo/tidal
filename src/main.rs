7aab15690bf49b1eedb4e4bdb0060592946e88bf
Brendan Kennedy <brenwken@gmail.com>
2020-10-27 18:09:02 -0400

    rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 use std::env;
 
+struct X264Args {}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let url = &args[1];
 
     println!("url: {}", url);
+
+    // ffmpeg get metadata
+
+    // create presets
+
+    // return json
 }
